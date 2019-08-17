6aa0e72695453d438917ebf2be2b6b803047f6a5
visual_community@outlook.com <visual_community@outlook.com>
2019-08-17 08:49:34 -0400

    con MVC Completo

---
+++ mcv/mcv/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace mcv.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<mcv.Models.Student> Students { get; set; }
+    }
+}
